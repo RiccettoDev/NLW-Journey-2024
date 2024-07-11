775db0ecfc549d03097f2f792c5dce40306d04fe
Eduardo <princesherlockk@gmail.com>
2024-07-11 16:04:17 -0300

    [NLW-Journey]-feat- installed email dependency

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,17 @@
 import fastify from 'fastify'
-import { prisma } from './lib/prisma'
+import { createTrip } from './routes/create-trip'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello NLW!'
-})
+// dependências do zod
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.get('/cadastrar', async () => {
-  await prisma.trip.create({
-    data: {
-      destination: 'Florianópolis',
-      starts_at: new Date(),
-      ends_at: new Date(),
-    },
-  })
+// rotas
 
-  return 'Registro cadastrado com sucesso!'
-})
-
-app.get('/listar', async () => {
-  const trips = await prisma.trip.findMany()
-
-  return trips
-})
+// rota de criação
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!!!');
