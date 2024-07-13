5db39150b8a4a70f2a50567d5fc68de054bac3bb
Eduardo <princesherlockk@gmail.com>
2024-07-13 11:07:22 -0300

    [NLW-Journey]-feat- created confirm route

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,15 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
-import dayjs from "dayjs";
-import localizedFormat from "dayjs/plugin/localizedFormat"
-import "dayjs/locale/pt-br"
 import nodemailer from "nodemailer"
 import { z } from "zod"
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { getMailClient } from "../lib/mail";
-
-dayjs.locale('pt-br')
-dayjs.extend(localizedFormat)
+import { dayjs } from "../lib/dayjs";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/trips', {
